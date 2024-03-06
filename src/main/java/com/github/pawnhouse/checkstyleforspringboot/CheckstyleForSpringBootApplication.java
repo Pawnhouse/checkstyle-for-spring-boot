3c745ea831cf661d26b6dee42fc5f2f488759602
Баяк Е.И. <e.baiak@bsuir.by>
2024-03-06 16:12:19 +0300

    initial commit

---
+++ src/main/java/com/github/pawnhouse/checkstyleforspringboot/CheckstyleForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.pawnhouse.checkstyleforspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckstyleForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckstyleForSpringBootApplication.class, args);
+	}
+
+}

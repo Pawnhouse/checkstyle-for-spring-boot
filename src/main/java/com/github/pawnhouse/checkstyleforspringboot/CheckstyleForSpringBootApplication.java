b0873f87550c06a4c9daaf3553b29638da0664df
e.baiak <e.baiak@bsuir.by>
2024-03-06 17:29:26 +0300

    format code

--- src/main/java/com/github/pawnhouse/checkstyleforspringboot/CheckstyleForSpringBootApplication.java
+++ src/main/java/com/github/pawnhouse/checkstyleforspringboot/CheckstyleForSpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CheckstyleForSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CheckstyleForSpringBootApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CheckstyleForSpringBootApplication.class, args);
+  }
 
 }
